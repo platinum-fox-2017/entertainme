c043484b859fe9c05cc05bf119b659191946f937
Ervan Adetya <vaanz.adetya@gmail.com>
2018-04-12 20:52:23 +0700

    Adding create cache helper

--- orkestrator/routes/index.js
+++ orkestrator/routes/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAll } = require('../controllers');
-const { checkCahce } = require('../middlewares/cache.middlewares')
-
-router.get('/', function(req, res) {
-  res.send('index', { title: 'Express' });
-});
+const { getAll, createMovie, createTv } = require('../controllers');
+const { checkCahce } = require('../middlewares/cache.middlewares');
 
 router.get('/entertainme', checkCahce,  getAll);
+
+router.post('/movie', checkCahce,  createMovie);
+router.post('/tv', checkCahce,  createTv);
 
 
 module.exports = router;
