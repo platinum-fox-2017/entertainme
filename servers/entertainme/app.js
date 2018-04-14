fb9c41ac4ec82a35d6ae0fa1a137f2a56aa1a136
Chandra Budiman <cbudiman1000@gmail.com>
2018-04-14 21:56:04 +0700

    With Redis

--- servers/entertainme/app.js
+++ servers/entertainme/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/entertainme', caching, entertainme);
+app.use('/entertainme', entertainme);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
