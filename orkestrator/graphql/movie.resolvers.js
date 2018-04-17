d79e0c6971d572a12431c0e958c34aabe0ab758d
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-04-17 11:45:50 +0700

    example mutation

--- orkestrator/graphql/movie.resolvers.js
+++ orkestrator/graphql/movie.resolvers.js
@@ -7,6 +7,13 @@ const resolvers = {
       const response = await axios.get('http://localhost:3001/movies')
       return response.data.data;
     }
+  },
+  Mutation: {
+    addMovie: async(_,{ movie }) => {
+      console.log(movie);
+      const response = await axios.post('http://localhost:3001/movies', movie)
+      return response.data
+    }
   }
 }
 
--- orkestrator/graphql/movies.js
+++ orkestrator/graphql/movies.js
@@ -1,9 +1,19 @@
 const typeDefs = `
   type Query { movies: [Movie] }
+
   type Movie {
     title: String
     overview: String,
     poster_path: String,
+  }
+
+  type Mutation {
+    addMovie(movie: MovieData): Movie
+  }
+
+  input MovieData {
+    title: String!
+    overview: String!
   }
 `
 
