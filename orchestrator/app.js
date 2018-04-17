92d2d235f467d664fa911ae0ff5140e8157292ee
Devdots <devdots@Devdotss-MBP.localdomain>
2018-04-17 11:08:33 +0700

    orchestrator with graphql

--- orchestrator/app.js
+++ orchestrator/app.js
@@ -4,12 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
+var fs = require('fs')
+var { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+var { makeExecutableSchema } = require('graphql-tools');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var orchestrator = require('./routes/orchestra');
 
 var app = express();
+app.use(cors())
+
+const typeDefs = fs.readFileSync('./graphql/orchestrator.gql', 'utf-8')
+const resolvers = require('./graphql/resolver')
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +38,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/orches', orchestrator);
+app.use('/graphql', graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
